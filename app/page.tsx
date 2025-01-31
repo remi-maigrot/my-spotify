45db9ca6f7821592117dfb6f7012db275fd79714
Rémi Maigrot <remi.maigrot10@gmail.com>
2025-01-31 14:35:47 +0100

    (fix) change name of projet

--- app/page.tsx
+++ app/page.tsx
@@ -360,7 +360,7 @@ export default function Home() {
     <div className="min-h-screen bg-gray-50 dark:bg-gray-900">
       <div className="container mx-auto p-6 pb-24">
         <h1 className="text-4xl font-bold mb-8 text-center bg-gradient-to-r from-primary to-blue-600 bg-clip-text text-transparent">
-          Découverte Musicale
+          MySpotify / Jonkoping University
         </h1>
         
         <div className="flex gap-4 mb-8">
